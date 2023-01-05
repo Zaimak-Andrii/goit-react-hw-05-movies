348e33c73551fb6549ecabd50006972119cb0a14
Andrii Zaimak <xiveres@MacBook-Air-Andrii.local>
2023-01-05 16:54:14 +0200

    Add message not found

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -11,39 +11,47 @@ export default function Reviews() {
   const data = useLoaderData();
 
   return (
-    <Grid
-      container
-      spacing={{ xs: 2, md: 3 }}
-      columns={{ xs: 4, sm: 8, md: 1 }}
-    >
-      {data.results.map(({ author, content, updated_at, id }) => (
-        <Grid item xs={2} sm={2} md={3} key={id}>
-          <Card sx={{ maxWidth: '100%', height: '100%' }}>
-            <CardActionArea>
-              <CardContent>
-                <Typography
-                  gutterBottom
-                  variant="body2"
-                  component="div"
-                  fontWeight="700"
-                >
-                  {author}{' '}
-                  <Typography
-                    variant="body2"
-                    component="span"
-                    color="text.secondary"
-                  >
-                    ({new Date(updated_at).toDateString()})
-                  </Typography>
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {content}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
+    <>
+      {data.results.length === 0 ? (
+        <Typography variant="body2" component="div">
+          Reviews not found
+        </Typography>
+      ) : (
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 1 }}
+        >
+          {data.results.map(({ author, content, updated_at, id }) => (
+            <Grid item xs={2} sm={2} md={3} key={id}>
+              <Card sx={{ maxWidth: '100%', height: '100%' }}>
+                <CardActionArea>
+                  <CardContent>
+                    <Typography
+                      gutterBottom
+                      variant="body2"
+                      component="div"
+                      fontWeight="700"
+                    >
+                      {author}{' '}
+                      <Typography
+                        variant="body2"
+                        component="span"
+                        color="text.secondary"
+                      >
+                        ({new Date(updated_at).toDateString()})
+                      </Typography>
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {content}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
         </Grid>
-      ))}
-    </Grid>
+      )}
+    </>
   );
 }
